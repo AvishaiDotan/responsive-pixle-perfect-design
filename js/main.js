7df9acf9f07c951b890fa2591965eaaac1e05ccf
Avishai Dotan <avishaiDotan@gmail.com>
2022-10-22 23:24:38 +0300

    add files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+'use strict'
+
+function onToggleMenu() {
+    const elNav = document.querySelector('.main-nav')
+    const elBody = document.querySelector('body')
+    const elBtn = document.querySelector('.open-menu-btn')
+
+    const isOpen = elNav.classList.contains('display-menu')
+
+    if (!isOpen) {
+        elBody.classList.add('menu-open')
+        elNav.classList.add('display-menu')
+        elBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+    } else {
+        elBody.classList.remove('menu-open')
+        elNav.classList.remove('display-menu')
+        elBtn.innerHTML = `<i class="fa-solid fa-burger"></i>`
+    }
+}
