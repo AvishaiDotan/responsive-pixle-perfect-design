5694809636718cf8a6f9ac301b95aad922e3afd4
Avishai Dotan <avishaiDotan@gmail.com>
2022-10-23 17:09:20 +0300

    Add existing project files to Git

---
+++ js/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+function onToggleMenu() {
+    const elNav = document.querySelector('.main-nav')
+    const elBody = document.querySelector('body')
+    const elBtn = document.querySelector('.open-menu-btn')
+
+    const isOpen = elNav.classList.contains('display-menu')
+
+    if (!isOpen) {
+        elBody.classList.add('menu-open')
+        elNav.classList.add('display-menu')
+        elBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+    } else {
+        elBody.classList.remove('menu-open')
+        elNav.classList.remove('display-menu')
+        elBtn.innerHTML = `<i class="fa-solid fa-burger"></i>`
+    }
+}
+
+function onOpenLink(elA) {
+    const elLinks = [...document.querySelectorAll('.nav-list li')]
+
+    elLinks.forEach(link => link.classList.remove('toggled'))
+
+    elA.classList.add('toggled')
+}
+
+function onSignNewsletter() {
+    const elBody = document.querySelector('body')
+    elBody.classList.add('open-sign-newsletter-modal')
+}
+
+function onCloseModal() {
+    const elBody = document.querySelector('body')
+    elBody.classList.remove('open-sign-newsletter-modal')
+}
